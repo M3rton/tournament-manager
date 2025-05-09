42e405a130028aa6f57726d231e06fe8156b0ad8
Marek Balota <xbalota@fi.muni.cz>
2025-05-09 22:08:01 +0200

    feat: add implemenation of App and MainWindow

---
+++ TournamentManager.WPF/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using TournamentManager.Core.Interfaces.Navigation;
+using TournamentManager.DataAccess;
+using TournamentManager.Services;
+using TournamentManager.ViewModels;
+using TournamentManager.ViewModels.ViewModels;
+using TournamentManager.WPF.Services;
+using TournamentManager.WPF.Views;
+
+namespace TournamentManager.WPF;
+
+public partial class App : Application
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRepositories();
+        services.AddServices();
+        services.AddViewModels();
+
+        services.AddSingleton(CreateViewModelToViewMap());
+        services.AddSingleton<IWindowManager, WindowManager>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var windowManager = _serviceProvider.GetRequiredService<IWindowManager>();
+        windowManager.ShowWindow<LoginWindowViewModel>(_ => { });
+
+        base.OnStartup(e);
+    }
+
+    private static Dictionary<Type, Type> CreateViewModelToViewMap()
+    {
+        return new Dictionary<Type, Type>
+        {
+            { typeof(MainWindowViewModel), typeof(MainWindow) },
+            { typeof(LoginWindowViewModel), typeof(LoginWindow) },
+            { typeof(PopUpWindowViewModel), typeof(PopUpWindow)},
+        };
+    }
+}
---
+++ TournamentManager.WPF/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TournamentManager.WPF;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
