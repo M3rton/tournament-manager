d95b8cb1649e8a85b0bc60de28913919eb7d8406
Marek Balota <xbalota@fi.muni.cz>
2025-05-09 21:58:23 +0200

    feat: add implementation of DbContext and DbContext Factory which is necessary for design time

---
+++ TournamentManager.DataAccess/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace TournamentManager.DataAccess.Data;
+
+internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TournamentDbContext>
+{
+    public TournamentDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<TournamentDbContext>();
+        var connectionString = "Data Source=DESKTOP-L4MMGR0\\SQLEXPRESS;Integrated Security=True;" +
+            "Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        builder.UseSqlServer(connectionString);
+
+        return new TournamentDbContext(builder.Options);
+    }
+}
---
+++ TournamentManager.DataAccess/Data/TournamentDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentManager.Core.Entities;
+using TournamentManager.DataAccess.Configurations;
+
+namespace TournamentManager.DataAccess.Data;
+
+internal class TournamentDbContext : DbContext
+{
+    public DbSet<Tournament> Tournaments { get; set; }
+
+    public DbSet<Team> Teams { get; set; }
+
+    public DbSet<Player> Players { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Match> Matches { get; set; }
+
+    public TournamentDbContext(DbContextOptions<TournamentDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TournamentConfiguration());
+        modelBuilder.ApplyConfiguration(new TeamConfiguration());
+        modelBuilder.ApplyConfiguration(new PlayerConfiguration());
+        modelBuilder.ApplyConfiguration(new MatchConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
