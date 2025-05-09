74a1b4f526580d4dee99c33aee1803fa2708fc29
Marek Balota <xbalota@fi.muni.cz>
2025-05-09 22:09:48 +0200

    feat: add implementation of LoginWindow

---
+++ TournamentManager.WPF/LoginWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TournamentManager.WPF;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
