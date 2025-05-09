a1ad23f9992e298f05e91ce496f8cf1027ebfbc2
Marek Balota <xbalota@fi.muni.cz>
2025-05-09 22:07:29 +0200

    feat: add implementation of WindowManager

---
+++ TournamentManager.WPF/Services/WindowManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using TournamentManager.Core.Interfaces.Navigation;
+
+namespace TournamentManager.WPF.Services;
+
+internal class WindowManager : IWindowManager
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly Dictionary<Type, Type> _viewModelToViewMap;
+    private readonly Dictionary<object, Window> _openWindows = new Dictionary<object, Window>();
+
+    public WindowManager(IServiceProvider serviceProvider, Dictionary<Type, Type> viewModelToViewMap)
+    {
+        _serviceProvider = serviceProvider;
+        _viewModelToViewMap = viewModelToViewMap;
+    }
+
+    public void ShowWindow<TViewModel>(Action<TViewModel> setup) where TViewModel : class
+    {
+        var viewModel = _serviceProvider.GetRequiredService<TViewModel>();
+
+        setup(viewModel);
+
+        if (_viewModelToViewMap.ContainsKey(viewModel.GetType()))
+        {
+            Type viewType = _viewModelToViewMap[viewModel.GetType()];
+            if (viewType == null)
+            {
+                return;
+            }
+
+            var window = (Window)Activator.CreateInstance(viewType)!;
+            window.DataContext = viewModel;
+            _openWindows[viewModel] = window;
+            window.Show();
+        }
+    }
+
+    public void CloseWindow<TViewModel>(TViewModel viewModel) where TViewModel : class
+    {
+        if (_openWindows.ContainsKey(viewModel))
+        {
+            var window = _openWindows[viewModel];
+
+            window.Close();
+            _openWindows.Remove(viewModel);
+        }
+    }
+}
