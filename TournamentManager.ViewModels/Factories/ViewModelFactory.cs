af2f0bb20c1dc7f9a22ece383c5424cdbe2ad117
Marek Balota <xbalota@fi.muni.cz>
2025-05-09 22:05:30 +0200

    feat: add implementation of ViewModel Factory which will be used later for the usage of Dependency Injector

---
+++ TournamentManager.ViewModels/Factories/ViewModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using TournamentManager.ViewModels.Interfaces;
+
+namespace TournamentManager.ViewModels.Factories;
+
+internal class ViewModelFactory<T> : IViewModelFactory<T>
+{
+    private readonly Func<T> _factory;
+
+    public ViewModelFactory(Func<T> factory)
+    {
+        _factory = factory;
+    }
+
+    public T Create()
+    {
+        return _factory();
+    }
+}
