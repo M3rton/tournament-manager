6c9928ddc212fc76d44f83b72c418e203e26a3a7
Marek Balota <xbalota@fi.muni.cz>
2025-05-12 04:35:46 +0200

    feat: add viewmodels for tournament view

---
+++ TournamentManager.ViewModels/ViewModels/CreateTournamentViewModel.cs
@@ -0,0 +1,104 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using TournamentManager.Core.Entities;
+using TournamentManager.Core.Enums;
+using TournamentManager.Core.Events;
+using TournamentManager.Core.Interfaces.Services;
+
+namespace TournamentManager.ViewModels.ViewModels;
+
+public partial class CreateTournamentViewModel : ObservableObject
+{
+    public Player? Player { get; set; }
+
+    private readonly ITournamentsService _tournamentsService;
+
+    public ObservableCollection<StrategyType> Strategies { get; set; } =
+        [.. Enum.GetValues(typeof(StrategyType)).Cast<StrategyType>()];
+
+    public ObservableCollection<int> MaxTeams { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(CreateTournamentCommand))]
+    private string? _tournamentName;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(CreateTournamentCommand))]
+    private StrategyType? _selectedStrategy;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(CreateTournamentCommand))]
+    private int? _selectedMaxTeams;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(CreateTournamentCommand))]
+    private string? _tournamentDescription;
+
+    private readonly PopUpMessageEvent _popUpMessageEvent;
+    private readonly ChangeViewModelEvent _changeViewModelEvent;
+
+    public CreateTournamentViewModel(ITournamentsService tournamentsService, IEventAggregator eventAggregator)
+    {
+        _tournamentsService = tournamentsService;
+
+        MaxTeams = new ObservableCollection<int>();
+        for (int i = 2; i <= 64; i*=2)
+        {
+            MaxTeams.Add(i);
+        }
+
+        _popUpMessageEvent = eventAggregator.GetEvent<PopUpMessageEvent>();
+        _changeViewModelEvent = eventAggregator.GetEvent<ChangeViewModelEvent>();
+    }
+
+    [RelayCommand (CanExecute =nameof(CanCreateTournament))]
+    private async Task CreateTournament()
+    {
+        string message;
+        bool teamCreated = false;
+
+        if (await _tournamentsService.CanCreateTournamentAsync(TournamentName!))
+        {
+            await _tournamentsService.CreateTournamentAsync(
+                TournamentName!,
+                SelectedStrategy,
+                SelectedMaxTeams,
+                TournamentDescription,
+                Player);
+
+            message = "Successfully created new tournament.";
+
+            teamCreated = true;
+        }
+        else
+        {
+            message = "Failed to create a tournament. This tournament name is already used.";
+        }
+
+        _popUpMessageEvent.Publish(new PopUpMessagePayload
+        {
+            Sender = this,
+            Message = message
+        });
+
+        if (teamCreated)
+        {
+            _changeViewModelEvent.Publish(new ChangeViewModelPayload
+            {
+                Sender = this,
+                ViewModelName = nameof(MyTournamentViewModel)
+            });
+        }
+    }
+
+    private bool CanCreateTournament()
+    {
+        if (string.IsNullOrEmpty(TournamentName) || SelectedMaxTeams == null || SelectedStrategy == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ TournamentManager.ViewModels/ViewModels/MyTournamentViewModel.cs
@@ -0,0 +1,81 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TournamentManager.Core.Entities;
+using TournamentManager.Core.Interfaces.Services;
+
+namespace TournamentManager.ViewModels.ViewModels;
+
+public partial class MyTournamentViewModel : ObservableObject
+{
+    public Player? Player { get; set; }
+
+    private readonly ITournamentsService _tournamentsService;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(AddTeamCommand))]
+    private string? _teamName;
+
+    public MyTournamentViewModel(ITournamentsService tournamentsService)
+    {
+        _tournamentsService = tournamentsService;
+    }
+
+    [RelayCommand]
+    private async Task GenerateBracket()
+    {
+        if (Player!.Tournament == null || Player != Player.Tournament.Owner)
+        {
+            return;
+        }
+        await _tournamentsService.GenerateBracketAsync(Player.Tournament);
+    }
+
+    [RelayCommand]
+    private async Task SaveMatch(Match match)
+    {
+        if (match.FirstTeamWins == match.SecondTeamWins)
+        {
+            return;
+        }
+
+        if (match.FirstTeamWins > match.SecondTeamWins)
+        {
+            match.WinnerTeam = match.FirstTeam;
+        }
+        else
+        {
+            match.WinnerTeam = match.SecondTeam;
+        }
+
+        match.IsFinished = true;
+    }
+
+    [RelayCommand]
+    private void ExportTournament()
+    {
+
+    }
+
+    [RelayCommand(CanExecute = nameof(CanAddTeam))]
+    private async Task AddTeam()
+    {
+        if (Player!.Tournament == null || Player != Player.Tournament.Owner || Player.Tournament.Matches.Count != 0)
+        {
+            return;
+        }
+
+        await _tournamentsService.AddTeamAsync(Player.Tournament, TeamName!);
+        OnPropertyChanged();
+    }
+
+    private bool CanAddTeam()
+    {
+        return !string.IsNullOrEmpty(TeamName);
+    }
+
+    [RelayCommand]
+    private void ExportBracket()
+    {
+
+    }
+}
