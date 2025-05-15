ea6f1575761a4f67a702b6cb7057df12286fe3f5
Marek Balota <xbalota@fi.muni.cz>
2025-05-15 21:24:14 +0200

    refactor: move options class and its builder from core to viewmodels

---
+++ TournamentManager.ViewModels/Options/ExportBracketAsImageOptions.cs
@@ -0,0 +1,23 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace TournamentManager.ViewModels.Options;
+
+public class ExportBracketAsImageOptions
+{
+    public int ImageWidthOffSet { get; set; } = 30;
+    public int ImageHeightOffSet { get; set; } = 30;
+    public int FontSize { get; set; } = 12;
+    public string FontName { get; set; } = "Arial";
+    public int SpaceBetweenRounds { get; set; } = 30;
+    public int SpaceBetweenMatches { get; set; } = 15;
+    public int TextPadding { get; set; } = 5;
+    public Color TextColor { get; set; } = new Color(Rgba32.ParseHex("#000000FF"));
+    public Color LinesColor { get; set; } = new Color(Rgba32.ParseHex("#000000FF"));
+    public Color BackgroundColor { get; set; } = new Color(Rgba32.ParseHex("#FFFFFFFF"));
+    public int LinesThickness { get; set; } = 3;
+
+    public ExportBracketAsImageOptions()
+    {
+    }
+}
---
+++ TournamentManager.ViewModels/Options/ExportBracketAsImageOptionsBuilder.cs
@@ -0,0 +1,81 @@
+﻿using SixLabors.ImageSharp;
+
+namespace TournamentManager.ViewModels.Options;
+
+public class ExportBracketAsImageOptionsBuilder
+{
+    private ExportBracketAsImageOptions _options;
+
+    public ExportBracketAsImageOptionsBuilder()
+    {
+        _options = new ExportBracketAsImageOptions();
+    }
+
+    public void Reset()
+    {
+        _options = new ExportBracketAsImageOptions();
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetFontSize(int fontSize)
+    {
+        _options.FontSize = fontSize;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetFontName(string fontName)
+    {
+        _options.FontName = fontName;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetSpaceBetweenRounds(int spaceBetweenRounds)
+    {
+        _options.SpaceBetweenRounds = spaceBetweenRounds;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetSpaceBetweenMatches(int spaceBetweenMatches)
+    {
+        _options.SpaceBetweenRounds = spaceBetweenMatches;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetTextPadding(int textPadding)
+    {
+        _options.TextPadding = textPadding;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetTextColor(Color textColor)
+    {
+        _options.TextColor = textColor;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetLinesColor(Color linesColor)
+    {
+        _options.LinesColor = linesColor;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetBackgroundColor(Color backgroundColor)
+    {
+        _options.BackgroundColor = backgroundColor;
+        return this;
+    }
+
+    public ExportBracketAsImageOptionsBuilder SetLinesThickness(int linesThickness)
+    {
+        _options.LinesThickness = linesThickness;
+        return this;
+    }
+
+    public ExportBracketAsImageOptions Build()
+    {
+        var result = _options;
+
+        Reset();
+
+        return result;
+    }
+}
