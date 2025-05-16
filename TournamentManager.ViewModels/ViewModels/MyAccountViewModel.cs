f03a137fcb1c3cb3c9f7f99ad45d502e1eaf4f36
Marek Balota <xbalota@fi.muni.cz>
2025-05-16 23:45:46 +0200

    feat: add implemenation of myaccountviewmodel

--- TournamentManager.ViewModels/ViewModels/MyAccountViewModel.cs
+++ TournamentManager.ViewModels/ViewModels/MyAccountViewModel.cs
@@ -1,9 +1,54 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using TournamentManager.Core.Entities;
+using TournamentManager.Core.Enums;
 
 namespace TournamentManager.ViewModels.ViewModels;
 
-public class MyAccountViewModel : ObservableObject
+public partial class MyAccountViewModel : ObservableObject
 {
-    public User? User { get; set; }
+    public Player? Player { get; set; }
+
+    public ObservableCollection<Role> Roles { get; set; } =
+        [.. Enum.GetValues(typeof(Role)).Cast<Role>()];
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveChangesCommand))]
+    private Role? _selectedMainRole;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveChangesCommand))]
+    private Role? _selectedSecondaryRole;
+
+    public MyAccountViewModel()
+    {
+    }
+
+    [RelayCommand (CanExecute = nameof(CanSaveChanges))]
+    private void SaveChanges()
+    {
+        if (Player == null)
+        {
+            return;
+        }
+
+        if (SelectedMainRole == null || SelectedSecondaryRole == null)
+        {
+            return;
+        }
+
+        Player.MainRole = (Role) SelectedMainRole;
+        Player.SecondaryRole = (Role) SelectedSecondaryRole;
+    }
+
+    private bool CanSaveChanges()
+    {
+        if (SelectedMainRole == null || SelectedSecondaryRole == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
